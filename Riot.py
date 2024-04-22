4adeeb3f0af4f4cef368f44de2a0eeb2983cbc91
as12sd23 <52819784+as12sd23@users.noreply.github.com>
2024-04-22 19:49:51 +0900

    Add files via upload

--- Riot.py
+++ Riot.py
@@ -1,10 +1,13 @@
 import requests
+import cv2
+import numpy as np
 
-ApiKey = "RGAPI-6bb0eed6-7bae-48c2-8d80-c8eefe7d8bfb"
+ApiKey = "RGAPI-75946a3e-2f2b-42aa-bc8f-4772ab2dda7d"
 header = {"X-Riot-Token": ApiKey}
-DeathPosition = {}
+DeathPosition = []
 GameColor = []
 Game = []
+
 
 #플레이어 검색
 def SummonerSearch(Tag, Name):
@@ -27,60 +30,93 @@ def MatchTimeLineData(matchId):
 
     return requests.get(url, headers = header)
 
-#플레이어 검색
-Account = SummonerSearch("KR1", "마루하늘")
+def PlayerSearch(name, tag):
+    
+    #플레이어 검색
+    Account = SummonerSearch(tag, name)
 
-#puuid 추출
-if Account.status_code == 200:
-    data = Account.json()
+    #puuid 추출
+    if Account.status_code == 200:
+        data = Account.json()
 
-    DataCode = data['puuid']
-    DataName = data['gameName']
+        DataCode = data['puuid']
 
-#인게임 검색
-GameSearch = MatchSearch(DataCode, 'normal', 20)
+        return DataCode
+    else
+        return 400
 
-if GameSearch.status_code == 200:
-    GameData = GameSearch.json()
-
-#인게임 데이터 분류
+def GameMatch(GameType,Count)
+    #인게임 검색
+    GameSearch = MatchSearch(DataCode, GameType, Count)
+    if GameSearch.status_code == 200:
+        GameData = GameSearch.json()
+        return GameData
+    else:
+        return 400
+    
+while True:
+    PlayerName = input("닉네임 검색")
+    if '#' in PlayerName:
+        Name = PlayerName.split('#')
+        _Number = PlayerSearch(Name[0], Name[1])
+        if _Number != 400:
+            GameType = input("1. 랭크\n2. 일반\n3. 자유랭\n > ")
+            GameCount = input("최대 100까지\n > ")
+            
+        else:
+            ("닉네임이 잘못되었습니다.")
+    else:
+        print("닉네임과 태그가 잘못 입력되었습니다.")
+    
+#타임라인
+deathmap = []
+killmap = []
 for i in GameData:
+    location = 0
     print('-----------------------------------')
     print(i)
-    InGame = MatchData(i)
-
+    InGame = MatchTimeLineData(i)
     if InGame.status_code == 200:
         InGameData = InGame.json()
-
-        InData = InGameData['info']
-        InData = InData['participants']
+        _InGameMetadata = InGameData['metadata']
+        _InGameParti = _InGameMetadata['participants']
+        for j in _InGameParti:
+            location += 1
+            if j == DataCode:
+                break
         
-        for i in InData:
-            if i['teamId'] == 100:
-                TeamColor = 'Blue'
-            elif i['teamId'] == 200:
-                TeamColor = 'Red'
-            TeamPosition = i['teamPosition']
-            print(TeamColor + "팀")
-            print("라인 :", TeamPosition)
-            print("이름 :", i['summonerName'])
+        _InGameData = InGameData['info']
+        _InGameDataEvent = _InGameData['frames']
 
-'''
-killerId가 킬 딴 사람 ID
-victimId가 죽은 사람 ID
+        for j in _InGameDataEvent:
+            DataEvent = j['events']
+            for k in DataEvent:
+                if 'victimId' in k:
+                    if k['victimId'] == location:
+                        deathmap.append(k['position'])
+                    if k['killerId'] == location:
+                        killmap.append(k['position'])
 
-matadata에서 participants 안에 순서로 번호가 결정됨
-내가 0번째면 1번 아이디를 가져감
+                        
 
-레드팀일 경우 6 ~ 10 숫자를 가져감
-블루팀일 경우 1 ~ 5 숫자를 가져감
+image = cv2.imread('C:/Users/c404/Desktop/sangjin/python/minimap.png')
+killImage = image.copy()
+deathImage = image.copy()
+#670 669 3
+#나누기 22 하면 될듯
+for i in killmap:
+    x = i['x'] / 24.39 + 34
+    y = i['y'] / 24.39 + 42
+    cv2.line(killImage, (int(x), int(y)), (int(x), int(y)), (0,255,0), 3)
 
-victimId칸 안에어 position에 x, y 좌표있음
-'''
+for i in deathmap:
+    x = i['x'] / 24.39 + 34
+    y = i['y'] / 24.39 + 42
+    cv2.line(deathImage, (int(x), int(y)), (int(x), int(y)), (0,0,255), 3)
 
-        
-        
-
-
-    
+cv2.imshow("kill", killImage)
+cv2.imshow("death", deathImage)
+cv2.waitKey()
+cv2.destroyAllWindows()
+                        
 
